cd5ca4f1e95c253b27a4d05b32f93fc1d9abce39
Mohammed506 <81885137+Mohammed506@users.noreply.github.com>
2022-01-13 14:57:46 +0300

    css is not changing

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../app.css">
+
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1>My First Post</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse placeat odio adipisci officiis omnis voluptates cupiditate assumenda ex possimus. Consequuntur facere ab possimus sunt atque! Excepturi officiis quae esse quidem.</p>
+    </article>
+
+</body>
+</html>
